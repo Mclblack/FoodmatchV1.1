a62c43a44d6e49f145b578a8a84486324054b082
Peter Lee <a01073594@se14l0120-15.ad.bcit.ca>
2019-04-12 16:13:35 -0700

    add steps page

---
+++ js/steps.js
@@ -0,0 +1,22 @@
+var selection = {
+    western : "img/icons/western.svg",
+    KJ : "img/icons/japan.svg",
+    indian : "img/icons/indian.svg",
+    veggie : "img/icons/%20veggie-icon.svg",
+    meat : "img/icons/meat-icon.svg",
+    noodles : "img/icons/noodles-icon.svg",
+    rice : "img/icons/rice-icon.svg",
+    soup : "img/icons/soup-icon.svg",
+    spciy : "img/icons/spicy-icon.svg"
+}
+
+var patterns = {
+    pt1 : "img/ct-patterns/pattern-1.svg",
+    pt2 : "img/ct-patterns/pattern-2.svg",
+    pt3 : "img/ct-patterns/pattern-3.svg",
+    pt4 : "img/ct-patterns/pattern-4.svg",
+    pt5 : "img/ct-patterns/pattern-5.svg",
+    pt6 : "img/ct-patterns/pattern-6.svg",
+    pt7 : "img/ct-patterns/pattern-7.svg",
+    
+}
