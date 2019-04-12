f16ec5dae0c41c275c7b0302a891d71949bda1c4
Peter Lee <a01073594@se14l0120-15.ad.bcit.ca>
2019-04-12 16:35:45 -0700

    proxy

--- js/steps.js
+++ js/steps.js
@@ -18,5 +18,73 @@ var patterns = {
     pt5 : "img/ct-patterns/pattern-5.svg",
     pt6 : "img/ct-patterns/pattern-6.svg",
     pt7 : "img/ct-patterns/pattern-7.svg",
+}
+
+var categories = {
+    indian1 : "Meat",
+    indian2 : "Veggie",
+//    meat1, veggie1 : "Spicy",
+//    meat2, veggie2 : "No Spicy"
+}
+
+/*=======   PKG  ==============*/
+
+var pkg = {
+    ctName1 : "",
+    ctName2 : "",
+    sel1 : "",
+    sel2 : "",
+    bgPt1 : "",
+    bgPt1 : ""
+}
+
+/*=======   HANDLER  =========*/
+
+var handler = {
+    set:function(obj, props, value){
+        if(props == "ctName1"){
+            ChangeCtNameUI1(value);
+        }
+        
+        if(props == "ctName2"){
+            ChangeCtNameUI2(value);
+        }
+        
+        if(props == "sel1"){
+            ChangeSel1IconUI(value);
+        }
+    }
+}
+
+var proxy = new Proxy(pkg, handler);
+
+/*=======  STATE FUNCTIONS   ========*/
+
+function Step1(el){
+    if(el.textContent == "Indian"){
+        pkg.ctName1 = categories.indian1;
+        pkg.ctName2 = categories.indian2;
+        pkg.sel1 = selection.indian;
+    }
     
+    proxy.ctName1 = pkg.ctName1;
+    proxy.ctName2 = pkg.ctName2;
+    proxy.sel1 = pkg.sel1;
+    
+    document.querySelector("#app1").style.display = "none";
+    document.querySelector("#app2").style.display = "grid";
+}
+
+/*=======   UI FUNCTIONS     ==========*/
+
+function ChangeCtNameUI1(val){
+    document.querySelector(".ca1").innerText = val;
+}
+
+function ChangeCtNameUI2(val){
+    document.querySelector(".ca2").innerText = val;
+}
+
+function ChangeSel1IconUI(val){
+    document.querySelector(".sel1").src = val;
 }
